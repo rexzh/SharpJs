e7d1cf6881d62576362aee1f394f2a6f73492093
rex <ling,zhang9@hp.com>
2016-01-15 13:17:53 +0800

    update test case.

--- Compiler/UnitTestSample_1/JsonObjectInitializer.cs
+++ Compiler/UnitTestSample_1/JsonObjectInitializer.cs
@@ -9,6 +9,77 @@ namespace UnitTestSample_1
         {
             var p = new Point() { x = 0, y = 1 };
         }
+
+        public void EmptyListInit()
+        {
+            var l = new System.Collections.ArrayList()
+            {
+            };
+        }
+
+        public void ListInit()
+        {
+            var l = new System.Collections.ArrayList()
+            {
+                1, 2, 3, 4
+            };
+        }
+
+        public void EmptyGenericListInit()
+        {
+            var l = new System.Collections.Generic.List<int>()
+            {
+            };
+        }
+
+        public void GenericListInit()
+        {
+            var l = new System.Collections.Generic.List<int>()
+            {
+                1, 2, 3, 4
+            };
+        }
+
+        public void EmptyHashTableInit()
+        {
+            var t = new System.Collections.Hashtable()
+            {
+            };
+        }
+
+        public void HashTableInit()
+        {
+            var t = new System.Collections.Hashtable()
+            {
+                {"x", 1 },
+                {"y", 2 }
+            };
+        }
+
+        public void EmptyDictionaryInit()
+        {
+            var t = new System.Collections.Generic.Dictionary<string, int>()
+            {
+            };
+        }
+
+        public void DictionaryInit()
+        {
+            var t = new System.Collections.Generic.Dictionary<string, int>()
+            {
+                {"x", 1 },
+                {"y", 2 }
+            };
+        }
+
+        public void DictionaryInit2()
+        {
+            var t = new System.Collections.Generic.Dictionary<string, int>()
+            {
+                ["x"] = 1,
+                ["y"] = 2
+            };
+        }
     }
 
     [NoCompile]
--- Compiler/UnitTestSample_1/ToCompare/JsonObjectInitializer.js
+++ Compiler/UnitTestSample_1/ToCompare/JsonObjectInitializer.js
@@ -4,5 +4,41 @@ enyo.kind({
 
     followClass : function(){
         var p = {"x": 0, "y": 1};
+    },
+
+    emptyListInit : function(){
+        var l = [];
+    },
+
+    listInit : function(){
+        var l = [1, 2, 3, 4];
+    },
+
+    emptyGenericListInit : function(){
+        var l = [];
+    },
+
+    genericListInit : function(){
+        var l = [1, 2, 3, 4];
+    },
+
+    emptyHashTableInit : function(){
+        var t = {};
+    },
+
+    hashTableInit : function(){
+        var t = {"x": 1, "y": 2};
+    },
+
+    emptyDictionaryInit : function(){
+        var t = {};
+    },
+
+    dictionaryInit : function(){
+        var t = {"x": 1, "y": 2};
+    },
+
+    dictionaryInit2 : function(){
+        var t = {"x": 1, "y": 2};
     }
 });
